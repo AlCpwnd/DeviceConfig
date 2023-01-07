5af16cc657ccc485db9591d6170a3c13888969b6
AlCapwnd <a.dejaeger@hotmail.com>
2023-01-07 23:29:22 +0100

    Further updates on the PS script

--- Automation/Config.ps1
+++ Automation/Config.ps1
@@ -1,22 +1,46 @@
 param(
-    [String]$ConfigPath
+    [Parameter(Mandatory,ParameterSetName='Cfg')][String]$ConfigPath,
+    [Parameter(Mandatory,ParameterSetName='Template')][Switch]$ExportConfig
 )
 
 #Requires -RunAsAdministrator
 
-# Defining Variables : ##########################
+<#=== File export ===#>
 
-## Device Brand
-$Brand = (Get-WMIObject -class Win32_ComputerSystem).Manufacturer
+if($ExportConfig){
+    [Xml]$Cfg = '<Config Name="BaseConfig">
+        <WindowsApps>
+            <BloatWareRemoval>True</BloatWareRemoval>
+            <AppInstallation Enabled="True">
+                <FilePath></FilePath>
+            </AppInstallation>
+            <AppUpdate>True</AppUpdate>
+        </WindowsApps>
+        <PowerOptions Eabled="True">
+            <DisableHybernate>True</DisableHybernate>
+            <Battery>
+                <DisplayTurnOff>30</DisplayTurnOff>
+                <SleepTimer>60</SleepTimer>
+            </Battery>
+            <Plugged>
+                <DisplayTurnOff>60</DisplayTurnOff>
+                <SleepTimer>0</SleepTimer>
+            </Plugged>
+        </PowerOptions>
+        <OfficeInstall>
+            <UninstallPreloaded>True</UninstallPreloaded>
+            <ConfigFile></ConfigFile>
+        </OfficeInstall>
+        <WindowsUpdates>True</WindowsUpdates>
+    </Config>'
+    $FilePath = "$PSScriptRoot\ConfigFile.xml"
+    $Cfg | Out-File -FilePath $FilePath
+    Write-Host "Config file exported to: $FIlePath"
+    return
+}
 
-## Configuration File Path
-if(!$ConfigPath){$ConfigPath = ".\Config.txt"}
 
-#################################################
-
-
-
-# Funtions : ####################################
+<#=== Functions ===#>
 
 function Remove-BloatWare{
     param(
@@ -47,45 +71,50 @@ function Install-Update{
     }
 }
 
+function show-info{
+    param([Parameter(Mandatory,Position=0)][String]$Txt)
+    $msg = "$(Get-Date -Format "[HH:MM:ss]"):$Txt"
+    Write-Host $msg
+    Add-Content -Path $Global:LogFile -Value $msg
+}
+
+function show-error{
+    param([Parameter(Mandatory,Position=0)][String]$Txt)
+    $Inverted = @{
+        ForegroundColor = $Host.UI.RawUI.BackgroundColor
+        BackgroundColor = $Host.UI.RawUI.ForegroundColor
+    }
+    $msg = "$(Get-Date -Format "[HH:MM:ss]"):[ERROR]:$Txt"
+    Write-Host $msg @Inverted
+    Add-Content -Path $Global:LogFile -Value $msg
+}
+
 #################################################
 
 
 
 # Script : ######################################
 
-## Configuration Preperation
-if(!(Test-Path -Path $ConfigPath)){
-    $Config = "#==========================#",
-        "#Script Configuration File:#",
-        "#==========================#",
-        "BloatwareRemoval=TRUE"
-        "Update=True",
-        "UpdateCount=0"
-        "OfficeCleanup=True",
-        "OfficeInstall=True",
-        "OfficeConfig=",
-        "Status=Start"
-    $Config | Out-File -FilePath $ConfigPath
-}else{
-    $Config = Get-Content -Path $ConfigPath -
+## Verifies if the given configruation file is valid
+try{
+    [xml]$Config = Get-Content $ConfigPath -ErrorAction Stop
+}catch{
+    Write-Host "Invalid config file: $ConfigPath" @Inverted
+    return
 }
 
-$cfg = [PSCustomObject]@{
-    Update = ''
-    RebootCount = 0
-    OfficeCleanup = ''
-    OfficeInstall = ''
-    OfficeConfig = ''
-    Status = ''
+
+$Global:LogFile = "$PSScriptRoot\Logs_$Env:COMPUTERNAME.txt"
+
+if(!(Test-Path $Global:LogFile)){
+    $SystemInfo = Get-WMIObject -class Win32_ComputerSystem
+    $Header = "| Date: $(Get-Date -Format dd/MM/yyyy)","|","| Config.ps1 log file.","|","| Serial N°: `t$((Get-WmiObject win32_bios).SerialNumber)","| Brand: `t$($SystemInfo.Brand)","| Model: `t$($SystemInfo.Model)","$("="*40)"
+    $Header | Out-File $Global:LogFile
+    show-info "Initiating script."
 }
 
-foreach($Entry in $Config){
-    if($Entry -match "#"){
-        Continue
-    }
-    $Temp = $Entry.Split("=")
-    $cfg.$Temp[0] = $Temp[1]
+if($Config.Config.WindowsApps.BloatWareRemoval){
+    Remove-BloatWare $SystemInfo.Brand
 }
-
 
 #################################################
--- Automation/Start.bat
+++ Automation/Start.bat
@@ -4,10 +4,10 @@ echo Administrative permissions required. Detecting permissions...
 rem Verifies if the script is running as admin.
 net session >nul 2>&1
 if %errorLevel% == 0 (
-    echo Success: Administrative permissions confirmed.
+    goto Continue
 ) else (
-    echo Failure: Current permissions inadequate.
-    pause
-    exit
+    powershell -command "Start-Process Start.bat -Verb runas"
 )
 
+:Continue
+echo Execution upgrade successfull
