ab71d14d223e131a7669ffc2bd7f4e535e47126b
AlCpwnd <a.dejaeger@hotmail.com>
2023-02-01 14:51:26 +0100

    Finalised the script. Testing on new devices.

--- UpdatePC.ps1
+++ UpdatePC.ps1
@@ -1,10 +1,9 @@
-#Requires -RunAsAdministrator
+# Requires -RunAsAdministrator
 
 # Verifies ExecutionPolicy
 if((Get-ExecutionPolicy) -ne "RemoteSigned"){
     Set-ExecutionPolicy RemoteSigned -Force
 }
-
 
 # Installing the module if not already installed.
 $ModuleCheck = Get-Module -Name PSWindowsUpdate -ListAvailable
@@ -29,12 +28,27 @@ if(!$ModuleCheck){
     }
 }
 
+# Generating post restart script run
 $LinkPath = "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\UpdatePC.lnk"
 $LinkTest = Test-Path $LinkPath
 if(!$LinkTest){
     $WshShell = New-Object -comObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($LinkPath)
     $Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-    $Shortcut.Arguments = "-NoLogo -NoExit -NoProfile -WindowStyle Maximised -File $PSCommandPath"
+    $Shortcut.Arguments = "-NoLogo -NoExit -NoProfile -WindowStyle Maximized -File $PSCommandPath"
     $Shortcut.Save()
+}
+
+# Updating device.
+try{
+    Import-Module PSWindowsUpdate
+    $AvailableUpdates = Get-WindowsUpdate
+    if($AvailableUpdates){
+        Install-WindowsUpdate -AcceptAll -AutoReboot -ErrorAction Stop
+    }else{
+        Remove-Item $LinkPath -Force
+        & cmd /c msg * "Device updates are done."
+    }
+}catch{
+    & cmd /c msg * "Error running the updates. Aborting script."
 }
