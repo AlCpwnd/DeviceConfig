6a1bd95009bf65cf78eb01bff94ae6bca609adf0
AlCpwnd <a.dejaeger@hotmail.com>
2023-02-09 11:51:39 +0100

    Script cleanup post tests and added clearer status messages

--- UpdatePC.ps1
+++ UpdatePC.ps1
@@ -1,6 +1,14 @@
 #Requires -RunAsAdministrator
 
+# Changes power options
+Write-Host "Configuring Power settings"
+& Powercfg /Change monitor-timeout-ac 60
+& Powercfg /Change monitor-timeout-dc 0
+& Powercfg /Change standby-timeout-ac 0
+& Powercfg /Change standby-timeout-dc 0
+
 # Disables UAC
+Write-Host "Disabling UAC"
 $RegistryItem = @{
     Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\"
     Name = "EnableLUA"
@@ -12,15 +20,17 @@ if((Get-ItemProperty @RegistryItem).EnableLUA){
 
 # Verifies ExecutionPolicy
 if((Get-ExecutionPolicy) -ne "RemoteSigned"){
+    Write-Host "Changing local execution policy"
     Set-ExecutionPolicy RemoteSigned -Force
 }
 
 # Installing the module if not already installed
 $ModuleCheck = Get-Module -Name PSWindowsUpdate -ListAvailable
 if(!$ModuleCheck){
-    $PackageProviderCheck = Get-PackageProvider -Name NuGet
-    if($PackageProviderCheck){
+    $PackageProviderCheck = Get-PackageProvider
+    if($PackageProviderCheck.Name -notcontains "NuGet"){
         try{
+            Write-Host "Installing package provider"
             Install-PackageProvider -Name NuGet -Force -ErrorAction Stop
         }catch{
             Write-Host "Failed to install required Package Provider `"Nuget`"" -ForegroundColor Red
@@ -29,8 +39,8 @@ if(!$ModuleCheck){
         }
     }
     try{
+        Write-Host "Installing PSWindowsUpdate module"
         Install-Module -Name PSWindowsUpdate -Force -ErrorAction Stop
-        Import-Module PSWindowsUpdate
     }catch{
         Write-Host "Failed to install module `"PSWindowsUpdate`"" -ForegroundColor Red
         Write-Host "Aborting script." -ForegroundColor Red
@@ -42,20 +52,24 @@ if(!$ModuleCheck){
 $LinkPath = "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\UpdatePC.lnk"
 $LinkTest = Test-Path $LinkPath
 if(!$LinkTest){
+    Write-Host "Creating the startup shortcut"
     $WshShell = New-Object -comObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($LinkPath)
     $Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-    $Shortcut.Arguments = "-NoLogo -NoExit -NoProfile -WindowStyle Maximized -File $PSCommandPath"
+    $Shortcut.Arguments = "-NoLogo -NoExit -NoProfile -File $PSCommandPath"
     $Shortcut.Save()
 }
 
 # Updating device
 try{
     Import-Module PSWindowsUpdate
+    Write-Host "Recovering available updates"
     $AvailableUpdates = Get-WindowsUpdate
     if($AvailableUpdates){
+        Write-Host "Installing updates"
         Install-WindowsUpdate -AcceptAll -AutoReboot -ErrorAction Stop
     }else{
+        Write-Host "Removing startup link"
         Remove-Item $LinkPath -Force
         & cmd /c msg * "Device updates are done."
     }
