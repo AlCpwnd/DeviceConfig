2f0db8c34785ab02e3d054ad2a3a47d190e1b679
AlCpwnd <a.dejaeger@hotmail.com>
2023-02-09 11:51:02 +0100

    File download isn't working at the moment, more testing required

--- Start.bat
+++ Start.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Administrative permissions required. Detecting permissions...
 
-SET PSScript = ".\UpdatePC.ps1"
+SET PSScript=".\UpdatePC.ps1"
 
 rem Verifies if the script is running as admin.
 net session >nul 2>&1
@@ -12,5 +12,5 @@ if %errorLevel% == 0 (
 )
 
 :Continue
-IF NOT EXIST %PSScript% powershell -NoLogo -NoExit -NoProfile -WindowStyle Maximized -ExecutionPolicy ByPass -Code {Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AlCpwnd/DeviceConfig/WindowsUpdateIntergration/UpdatePC.ps1" -UseBasicParsing -OutFile %PSScript%;& UpdatePC.ps1}
+IF NOT EXIST %PSScript% powershell -NoLogo -NoExit -NoProfile -ExecutionPolicy ByPass -Command {Invoke-RestMethod -Uri "https://raw.githubusercontent.com/AlCpwnd/DeviceConfig/WindowsUpdateIntergration/UpdatePC.ps1" -UseBasicParsing -OutFile %PSScript%}
 pause
